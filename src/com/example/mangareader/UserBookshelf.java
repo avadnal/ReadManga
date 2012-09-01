0a940625a05d03b4050e9673152a369b2e2cf974
avadnal <worked13@gmail.com>
2012-09-01 15:30:00 +1000

    test

---
+++ src/com/example/mangareader/UserBookshelf.java
@@ -0,0 +1,22 @@
+package com.example.mangareader;
+
+import roboguice.activity.RoboActivity;
+import roboguice.inject.ContentView;
+import android.os.Bundle;
+import android.view.Menu;
+
+@ContentView(R.layout.activity_user_bookshelf)
+public class UserBookshelf extends RoboActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_user_bookshelf, menu);
+        
+        return true;
+    }
+}
